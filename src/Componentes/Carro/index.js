b4689e1cec641dd6fb80648dc0050ac3a38304a8
Felipeferrazamaral <felipeferrazamaral@gmail.com>
2023-05-22 21:16:16 -0300

    att trabalho

--- src/Componentes/Carro/index.js
+++ src/Componentes/Carro/index.js
@@ -1,15 +1,28 @@
 import { CarroContainer } from './styles'
 
-export function Carro({ modelo, cor, ano, flex, adicionadoPor }) {
+export function Carro({
+  modelo,
+  cor,
+  ano,
+  flex,
+  adicionadoPor,
+  novoAutomovel,
+  setAutomovel
+}) {
+  const alterarAutomovel = () => {
+    setAutomovel(novoAutomovel);
+  };
+
   return (
     <CarroContainer>
       <h2>{modelo}</h2>
       <ul>
         <li>Cor: {cor}</li>
         <li>Ano: {ano}</li>
-        <li>Flex: {flex ? 'Sim' : 'Não'}</li>
+        <li>Flex: {flex ? "Sim" : "Não"}</li>
         <li>Adicionado: {adicionadoPor}</li>
       </ul>
+      <button onClick={alterarAutomovel}>Alterar Carro</button>
     </CarroContainer>
-  )
+  );
 }
